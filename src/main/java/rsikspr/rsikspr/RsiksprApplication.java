306882726c3bafe148ce59dfbdb2aaf4f7a8ee29
Luka Zekan <luka.zekan983@gmail.com>
2023-03-29 16:42:27 +0200

    rjesenje 1. labosa

--- src/main/java/rsikspr/rsikspr/RsiksprApplication.java
+++ src/main/java/rsikspr/rsikspr/RsiksprApplication.java
@@ -2,12 +2,22 @@ package rsikspr.rsikspr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class RsiksprApplication {
+
+	@GetMapping("/health")
+	public String getStatus() {
+		return "OK!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RsiksprApplication.class, args);
 	}
+
 
 }
