095f5fef1f00186637e4f2e99a1b77782528246c
Luka Zekan <luka.zekan983@gmail.com>
2023-03-29 14:03:43 +0200

    initial commit

---
+++ src/main/java/rsikspr/rsikspr/RsiksprApplication.java
@@ -0,0 +1,13 @@
+package rsikspr.rsikspr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsiksprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsiksprApplication.class, args);
+	}
+
+}
